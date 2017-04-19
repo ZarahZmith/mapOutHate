85aaa069fc78aa78bee8dc00aaa58ca43c4b359f
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-04-19 10:51:15 -0400

    Created ability to create a new incident report.

--- server/routes/reports.routes.js
+++ server/routes/reports.routes.js
@@ -1,7 +1,29 @@
+'use strict';
+
 const reportsRouter = require('express').Router();
 const Report = require('../models/Report.model');
 
 function addAReport(req, res, next) {
-
+  let newReport = new Report({
+    type: req.body.type,
+    description: req.body.description,
+    address: req.body.address,
+    city: req.body.city,
+    state: req.body.state,
+    zip: req.body.zip
+  });
+  newReport.createTime = Date.now();
+  newReport.save()
+    .then(function makeNewReport(content) {
+      res.json(content);
+    })
+    .catch(function errorHandler(err) {
+      console.error(err);
+      let theError = new Error('Could not save the report to the datanase');
+      theError.status = 500;
+      return next(theError);
+    });
 }
 reportsRouter.post('/', addAReport);
+
+module.exports = reportsRouter;
