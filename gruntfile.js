39a5390021b803b5f21eb95e77fffd1086737485
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-04-19 15:49:09 -0400

    Corrected source for jshint

--- gruntfile.js
+++ gruntfile.js
@@ -41,7 +41,7 @@ module.exports = function(config) {
           jshintrc: '.jshintrc'
         },
         files: {
-          src: ['src/**/*.js']
+          src: ['client/js/**/*.js']
         }
       }
     },
