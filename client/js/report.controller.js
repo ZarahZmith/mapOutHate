336d1d8235671eab8548d66218071f1ff1ae5488
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-04-24 13:42:33 -0400

    Removed console logs.

--- client/js/report.controller.js
+++ client/js/report.controller.js
@@ -12,10 +12,7 @@
     vm.notification = null;
     vm.content = {};
 
-    console.log('Report controller');
-
     vm.addReport = function addReport(content) {
-      console.log('add a report function', content);
       ReportService.addReport(content)
         .then(function addReportToPage() {
           vm.notification = 'Your report has sucessfully gone through. Thank you for sharing your story.';
