873823dff90ffd8f5a3491de4dcb2e37aeeaf23c
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-04-25 18:14:29 -0400

    Added docblock. Saved authorization token to local storage.

--- client/js/login.controller.js
+++ client/js/login.controller.js
@@ -10,11 +10,15 @@
 
     //used "window" here because facebook did not recognize "vm" (as it relates to the line "let vm = this;")
     window.getLoginStatus = function getLoginStatus() {
-
+      /**
+       * Retreives a the log in status from Facebook
+       * @param  {Object} response The information returned once the user attempts to log in
+       * @return {void}
+       */
       FB.getLoginStatus(function(response) {
         if (response.status === 'connected') {
           $state.go('report-incident');
-          //TODO save the token; response.authResponse.accessToken
+          localStorage.setItem('token', response.authResponse.accessToken);
         } else {
           console.error('Unable to get token. User is not signed in.');
         }
