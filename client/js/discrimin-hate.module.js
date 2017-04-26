ea707c32fc5781182cb3aaad9729a72a92b5503c
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-04-26 11:27:44 -0400

    Created function to verify login and redirect users home if token not provided. Added doc blocks.

--- client/js/discrimin-hate.module.js
+++ client/js/discrimin-hate.module.js
@@ -2,9 +2,16 @@
   'use strict';
 
   angular.module('discriminHATE', ['ui.router'])
-    .config(routerConfig);
+    .config(routerConfig)
+    .run(setupAuthCheck);
 
   routerConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+  /**
+   * Configures the router
+   * @param  {Object} $stateProvider     Allows for redirections to alternative templates in the app
+   * @param  {Object} $urlRouterProvider Monitors location via url
+   * @return {void}
+   */
   function routerConfig($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.when('', '/');
 
@@ -21,8 +28,26 @@
         url: '/report',
         templateUrl: '/templates/report-incident.template.html',
         controller: 'ReportController',
-        controllerAs: 'reportCtrl'
+        controllerAs: 'reportCtrl',
+        loginRequired: true
       });
 
   }
+
+  setupAuthCheck.$inject = ['$rootscope', '$state'];
+  /**
+   * Checks whether user is logged in or not
+   * @param  {Object} $rootscope  Lives on the window and gives access to any '$scope'
+   * @param  {Object} $state      Allows for a statechange
+   * @return {void}
+   */
+  function setupAuthCheck($rootscope, $state) {
+    $rootscope.$on('$stateChangeStart', function checkLoginStatus(eventObj, toState) {
+      if ( toState.loginRequired && !localStorage.getItem('token') ) {
+        eventObj.preventDefault();
+        $state.go('home');
+      }
+    });
+  }
+
 }());
