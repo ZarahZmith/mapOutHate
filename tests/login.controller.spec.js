072010ab416a44ae031d3446ffa9ab3217ab6936
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-04-26 13:13:37 -0400

    Created a test to make sure the get login fn does what it needs to do.

--- tests/login.controller.spec.js
+++ tests/login.controller.spec.js
@@ -11,13 +11,27 @@
 
     beforeEach(inject(function($controller) {
       LoginController = $controller('LoginController');
+
+      window.FB = {};
+
+      window.FB.getLoginStatus = function getLoginStatus() {
+        window.FB.getLoginStatus.numTimesCalled++;
+      };
+      window.FB.getLoginStatus.numTimesCalled = 0;
     }));
 
     describe('getLoginStatus', function() {
 
+
       it('should be a function', function() {
         console.info('window.getLoginStatus', window.getLoginStatus);
         expect(window.getLoginStatus).to.be.a('function');
+      });
+
+      it('should do what it is supposed to do', function () {
+        window.getLoginStatus();
+        expect(window.FB.getLoginStatus.numTimesCalled).to.equal(1);
+        //TODO check local storage for the token
       });
 
     });
