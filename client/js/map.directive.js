1abfd0d6a73686f8b08f356b63a0b3520a554fbc
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-04-27 17:23:46 -0400

    Created individual markers for each report and displays them all.

--- client/js/map.directive.js
+++ client/js/map.directive.js
@@ -9,12 +9,30 @@
     return {
       templateUrl: 'templates/map.template.html',
       restrict: 'E',
-      link: setupMap
+      link: setupMap,
+      scope: {
+        reports: '='
+      }
     };
 
-    function setupMap() {
-      new google.maps.Map(document.querySelector('#map'),
+    function setupMap(scope) {
+      let reportMap = new google.maps.Map(document.querySelector('#map'),
         { center: {lat: 38.9072, lng: -77.0369}, zoom: 8 });
+
+      scope.$watch('reports', function addReportsToMap() {
+        scope.reports.map(function addMarker(reportObj) {
+          console.log(reportObj, 'creating a marker for a report');
+
+          let reportLocation = new google.maps.LatLng(reportObj.latitude, reportObj.longitude);
+          new google.maps.Marker({
+            type: reportObj.type,
+            map: reportMap,
+            position: reportLocation
+          });
+        });
+      });
     }
+
+
   }
 }());
