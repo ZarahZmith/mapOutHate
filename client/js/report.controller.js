6e7eca41dfa23d8054e4e07e949d73b5c4c3a942
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-04-27 15:46:46 -0400

    Created a function to view all of the reports via the service. Added doc blocks.

--- client/js/report.controller.js
+++ client/js/report.controller.js
@@ -11,7 +11,13 @@
 
     vm.notification = $stateParams.successMessage;
     vm.content = {};
+    vm.reports = [];
 
+    /**
+     * Add a new report
+     * @param  {Object} content The data the report is made up of
+     * @return {void}
+     */
     vm.addReport = function addReport(content) {
       ReportService.addReport(content)
         .then(function addReportToPage() {
@@ -22,5 +28,25 @@
           vm.notification = 'Your report did NOT go through. Please try again.';
         });
     };
+
+    /**
+     * Retreive all reports
+     * @return {void}
+     */
+    function getReports() {
+      ReportService.viewAllReports()
+        .then(function retreiveReports(content) {
+          vm.reports = vm.reports.concat(content);
+          console.log(vm.reports);
+        })
+        .catch(function handleErrors(err) {
+          console.warn(err);
+          //TODO display error message to user
+        });
+    }
+
+    if ($state.current.name === 'view-reports') {
+      getReports();
+    }
   }
 }());
