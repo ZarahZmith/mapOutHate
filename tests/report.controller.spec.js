2d8d2919c367bf5bb71aff23cb2cd5f3c4e0471c
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-04-28 11:18:28 -0400

    Created mock  and tested first portion of the asyncronous addReport fn.

--- tests/report.controller.spec.js
+++ tests/report.controller.spec.js
@@ -7,24 +7,26 @@
 
     let ReportController;
     let mockReportService = {};
+    let mockState = {};
     let $stateParams = {};
 
     beforeEach(module('mapOutHate'));
 
     beforeEach(module(function($provide) {
       $provide.value('ReportService', mockReportService);
+      $provide.value('$state', mockState);
     }));
 
     beforeEach(inject(function($controller) {
       mockReportService.addReport = function addReport() {
-        return {
+        return Promise.resolve({
           type: 'religious',
           description: 'excessively burdoned at an airport',
           address: '1800 Address Street',
           city: 'Washington',
           state: 'D.C.',
           zip: '20008',
-        };
+        });
       };
 
       mockReportService.viewAllReports = function viewAllReports() {
@@ -44,10 +46,14 @@
         ];
       };
 
-      //TODO mock$state
+      mockState.current = {name: null};
+      mockState.go = function go() {
+        mockState.go.numTimesCalled++;
+      };
+      mockState.go.numTimesCalled = 0;
 
       $stateParams = {successMessage: null};
-      
+
       ReportController = $controller('ReportController', {$stateParams: $stateParams});
     }));
 
@@ -55,10 +61,21 @@
       expect(ReportController).to.be.an('object');
       expect(ReportController.notification).to.equal($stateParams.successMessage);
       expect(ReportController.content).to.be.an('object');
-      expect(ReportController.addReport).to.be.a('function');
       expect(ReportController.getReports).to.be.a('function');
     });
 
+    describe('addReport', function() {
+
+      it('should act the way it is intended to', function(done) {
+        expect(ReportController.addReport).to.be.a('function');
+        ReportController.addReport()
+          .then(function() {
+            expect(mockState.go.numTimesCalled).to.equal(1);
+            done();
+          });
+      });
+
+    });
     //TODO execute functions and ensure they do what they are supposed to
 
   });
