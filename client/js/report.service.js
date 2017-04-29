a1b194ad2038e9ff362d071d1d0a317f147285c5
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-04-29 13:50:30 -0400

    Attempted to add getFurtherAction function to connect information to the report controller. WIP.

--- client/js/report.service.js
+++ client/js/report.service.js
@@ -9,6 +9,27 @@
   function ReportService($http) {
 
     /**
+     * Function to connect to resources that provide steps for further action
+     * @param  {Object} type The type of discrimination
+     * @return {String}      Future step options
+     */
+    function takeFurtherAction(type) {
+      if (typeof(content) === racial) {
+        return 'To take further action contact the Anit-Defamation League (https://www.adl.org/take-action/report-an-incident) or the American Civil Liberties Union (https://www.aclu.org/about/affiliates).';
+      } else if (typeof(content) === religion) {
+        return 'To take further action contact the Anit-Defamation League (https://www.adl.org/take-action/report-an-incident).';
+      } else if (typeof(content) === sexual-orientation || typeof(content) === transgender) {
+        return 'To take further action report to the American Civil Liberties Union (https://action.aclu.org/secure/report-lgbthiv-discrimination).';
+      } else if (typeof(content) === gender) {
+        return 'To take the next step know your rights as presented via the U.S. Equal Employment Opprotunity Commission (https://www.eeoc.gov/laws/types/sex.cfm).'
+      } else if (typeof(content) === disability) {
+        return 'To take further action file a complaint with the Department of Justice (https://www.justice.gov/crt/how-file-complaint).';
+      } else if (typeof(content) === preganancy) {
+        return 'To take further action know your rights as presented via the U.S. Equal Employment Opprotunity Commission (https://www.eeoc.gov/laws/guidance/pregnancy_guidance.cfm).';
+      }
+    }
+
+    /**
      * Add a report to the database
      * @param {Object} content The data required in a report
      * @return {Promise}
@@ -49,7 +70,8 @@
 
     return {
       addReport: addReport,
-      viewAllReports: viewAllReports
+      viewAllReports: viewAllReports,
+      takeFurtherAction: takeFurtherAction
     };
   }
 
