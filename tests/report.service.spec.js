c445ccbe4b0548b174ec88fcf32242e7d9d80f1f
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-05-02 17:30:07 -0400

    Tested portions data[0] to ensure it return what it was expected to.

--- tests/report.service.spec.js
+++ tests/report.service.spec.js
@@ -122,6 +122,12 @@
           .then(function(data) {
             expect(data).to.be.a('array');
             expect(data[0]).to.be.an('object');
+            expect(data[0].type).to.be.a('string');
+            expect(data[0].description).to.be.a('string');
+            expect(data[0].address).to.be.a('string');
+            expect(data[0].city).to.be.a('string');
+            expect(data[0].state).to.be.a('string');
+            expect(data[0].zip).to.be.a('string');
             done();
           })
           .catch(function(err) {
