98ef4941ae4a8a35e87489c49c79d2856a5d38bc
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-05-02 16:54:57 -0400

    Mocked  get. Tested viewAllReports was a fn. Tested the returnValue contained an array of data.

--- tests/report.service.spec.js
+++ tests/report.service.spec.js
@@ -26,6 +26,21 @@
           createTime: '2017-03-10T00:09:16.445Z',
           id: '45tyhjwefhy57'
         });
+
+      $httpBackend
+        .whenGET('/report/all')
+        .respond([
+          {
+            type: 'religious',
+            description: 'excessively burdoned at an airport',
+            address: '1800 Address Street',
+            city: 'Washington',
+            state: 'D.C.',
+            zip: '20008',
+            createTime: '2017-03-10T00:09:16.445Z',
+            id: '45tyhjwefhy57'
+          }
+        ]);
     }));
 
     describe('addReport', function() {
@@ -78,6 +93,30 @@
         expect(result).to.equal('To take further action review your rights on the U.S. Equal Employment Opprotunity Commission website linked on the "More Information" page.');
         result = ReportService.takeFurtherAction('other');
         expect(result).to.equal('To take further action please refer to the "More Information" page.');
+      });
+
+    });
+
+    describe('viewAllReports', function() {
+
+      it('should be a function', function() {
+        expect(ReportService.viewAllReports).to.be.a('function');
+      });
+
+      it('should do what it is expected to', function() {
+        let returnValue = ReportService.viewAllReports([{
+          type: 'religious',
+          description: 'testing testing 123',
+          address: '1800 address Street',
+          city: 'Washington',
+          state: 'D.C.',
+          zip: '30000',
+        }]);
+
+        returnValue
+          .then(function(data) {
+            expect(data).to.be.a('array');
+          });
       });
 
     });
