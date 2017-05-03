92026026abab0a6887f1f88b35c9d4510e76adbb
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-05-03 10:21:25 -0400

    Changed location to MONGODB_URI.

--- server/database-setup.js
+++ server/database-setup.js
@@ -2,12 +2,12 @@
 
 const mongoose = require('mongoose');
 
-if (!process.env.MY_DB_LOCATION) {
+if (!process.env.MONGODB_URI) {
   console.error('No database detected!');
   process.exit(1046);
 }
 
-mongoose.connect(process.env.MY_DB_LOCATION);
+mongoose.connect(process.env.MONGODB_URI);
 
 mongoose.connection.on('error', function handleDBError(err) {
   console.error('Database error detected!', err);
