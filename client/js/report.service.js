9a09eece5a0e8c222d7a7c426aebe60d88bec3ad
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-05-04 09:42:36 -0400

    Capilalized types to reflect their values on the form.

--- client/js/report.service.js
+++ client/js/report.service.js
@@ -16,15 +16,15 @@
     function takeFurtherAction(type) {
       if (type === 'race') {
         return 'To take further action contact the Anti-Defamation League or the American Civil Liberties Union on the "More Information" page.';
-      } else if (type === 'religion') {
+      } else if (type === 'Religion') {
         return 'To take further action contact the Anit-Defamation League on the "More Information" page.';
-      } else if (type === 'sexual-orientation' || type === 'transgender') {
+      } else if (type === 'Sexual-orientation' || type === 'Transgender') {
         return 'To take further action report to the American Civil Liberties Union on the "More Information" page.';
-      } else if (type === 'gender') {
+      } else if (type === 'Gender') {
         return 'To take the next step know your rights as presented via the U.S. Equal Employment Opprotunity Commission on the "More Information" page.';
-      } else if (type === 'disability') {
+      } else if (type === 'Disability') {
         return 'To take further action file a complaint with the Department of Justice through the "More Information" page.';
-      } else if (type === 'pregnancy') {
+      } else if (type === 'Pregnancy') {
         return 'To take further action review your rights on the U.S. Equal Employment Opprotunity Commission website linked on the "More Information" page.';
       } else {
         return 'To take further action please refer to the "More Information" page.';
