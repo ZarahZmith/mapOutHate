09e44e42ae7d0ec38b884227de500e400e6b028a
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-05-04 09:46:48 -0400

    Capitalized Race.

--- client/js/report.service.js
+++ client/js/report.service.js
@@ -14,7 +14,7 @@
      * @return {String}      Future step options
      */
     function takeFurtherAction(type) {
-      if (type === 'race') {
+      if (type === 'Race') {
         return 'To take further action contact the Anti-Defamation League or the American Civil Liberties Union on the "More Information" page.';
       } else if (type === 'Religion') {
         return 'To take further action contact the Anit-Defamation League on the "More Information" page.';
